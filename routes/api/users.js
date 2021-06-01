449d5fa5f23bef0034f12f49affe0bd4765bb8d7
Chujun Lu <chujunlu@hotmail.com>
2021-06-01 21:15:44 +0800

    Create test routes

---
+++ routes/api/users.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+// @route   POST api/users
+// @desc    Register user
+// @access  Public
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty()
+  ],
+  (req, res) => {
+    console.log(req.body);
+    res.send('User route');
+  }
+);
+
+module.exports = router;
