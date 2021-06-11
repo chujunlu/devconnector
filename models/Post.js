8a3b9c980420e999011558a5cd08d9bffb752a61
Chujun Lu <chujunlu@hotmail.com>
2021-06-11 21:14:40 +0800

    Create post model

---
+++ models/Post.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    // The name for the user. If a user deletes account, their post can still be kept, and have a user name and avatar attached.
+    name: {
+        type: String
+    },
+    avatar: {
+        type: String
+    },
+    likes: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'user'
+            }
+        }
+    ],
+    comments: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'user'
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+            avatar: {
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Post = mongoose.model('post', PostSchema);
