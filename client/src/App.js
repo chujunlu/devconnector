321b031617a2ba91c72d12ada022d41d5ed80e9f
Chujun Lu <chujunlu@hotmail.com>
2021-06-22 21:45:33 +0800

    Client side setup

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
+const App = () => (
+    <>
+        <Navbar></Navbar>
+        <Landing></Landing>
+    </>
+);
+    
+export default App;
