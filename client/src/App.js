e14d54baf68f0b3ef7658bd48b780e44396e98b3
Chujun Lu <chujunlu@hotmail.com>
2021-06-22 22:27:34 +0800

    Set up routes for landing, register and login components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
-import './App.css';
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Landing from './components/layout/Landing'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+import './App.css'
 
 const App = () => (
-    <>
-        <Navbar></Navbar>
-        <Landing></Landing>
-    </>
+    <Router>
+        <>
+            <Navbar></Navbar>
+            <Route exact path="/" component={Landing} />
+            <section className="container">
+                <Switch>
+                    <Route exact path='/register' component={Register} />
+                    <Route exact path='/login' component={Login} />
+                </Switch>
+            </section>
+        </>
+    </Router>
+    
 );
     
 export default App;
