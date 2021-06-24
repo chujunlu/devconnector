158fce509d4bb22dc0b48343713ad20cdb7e8e8d
Chujun Lu <chujunlu@hotmail.com>
2021-06-24 09:23:28 +0800

    Try out making a request, thatwill be remove later

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { useState }from 'react'
+import axios from 'axios'
 
 const Register = () => {
     const [formData, setFormData] = useState({
@@ -15,13 +16,30 @@ const Register = () => {
         [e.target.name]: e.target.value
     });
 
-    const onSubmit = e => {
+    const onSubmit = async e => {
         e.preventDefault();
 
         if (password !== password2) {
             console.log('Passwords do not match');
         } else {
-            console.log(formData);
+            const newUser = {
+                name,
+                email,
+                password
+            }
+
+            try {
+                const config = {
+                    header: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+
+                const res = await axios.post('/api/users', newUser, config);
+                console.log(res.data);
+            } catch (err) {
+                console.error(err.response.data);
+            }
         }
     }
 
