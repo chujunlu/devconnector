e4a8b4363392a763fdd6cd0f4c09edea8c7ec028
Chujun Lu <chujunlu@hotmail.com>
2021-07-01 22:36:04 +0800

    Set token in axios header and get user by token

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
@@ -7,25 +8,37 @@ import Register from './components/auth/Register'
 // Redux
 import { Provider } from 'react-redux'
 import store from './store'
+import { loadUser } from './actions/auth'
 
+import setAuthToken from './utils/setAuthToken'
 import './App.css'
 
-const App = () => (
-    <Provider store={store}>
-        <Router>
-            <>
-                <Navbar></Navbar>
-                <Route exact path="/" component={Landing} />
-                <section className="container">
-                    <Alert />
-                    <Switch>
-                        <Route exact path='/register' component={Register} />
-                        <Route exact path='/login' component={Login} />
-                    </Switch>
-                </section>
-            </>
-        </Router> 
-    </Provider>
-);
+if (localStorage.token) {
+    setAuthToken(localStorage.token);
+}
+
+const App = () => {
+    useEffect(() => {
+        store.dispatch(loadUser());
+    }, []);
+
+    return (
+        <Provider store={store}>
+            <Router>
+                <>
+                    <Navbar></Navbar>
+                    <Route exact path="/" component={Landing} />
+                    <section className="container">
+                        <Alert />
+                        <Switch>
+                            <Route exact path='/register' component={Register} />
+                            <Route exact path='/login' component={Login} />
+                        </Switch>
+                    </section>
+                </>
+            </Router> 
+        </Provider>
+    )
+};
     
 export default App;
