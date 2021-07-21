83d1c617eac8749071194a7bbe6fcf741bebf928
Chujun Lu <chujunlu@hotmail.com>
2021-07-21 20:30:14 +0800

    Add create profile component and action

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
+import { Link, withRouter } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
-const CreateProfile = props => {
+import { createProfile } from '../../actions/profile'
+
+
+const CreateProfile = ({ createProfile, history }) => {
     const [formData, setFormData] = useState({
         company: '',
         website: '',
@@ -40,6 +44,11 @@ const CreateProfile = props => {
         [e.target.name]: e.target.value
     });
 
+    const onSubmit = e => {
+        e.preventDefault();
+        createProfile(formData, history)
+    }
+
     return (
         <>
             <h1 className="large text-primary">
@@ -50,7 +59,7 @@ const CreateProfile = props => {
                 profile stand out
             </p>
             <small>* = required field</small>
-            <form className="form">
+            <form className="form" onSubmit={e => onSubmit(e)}>
                 <div className="form-group">
                     <select name="status" value={status} onChange={e => onChange(e)}>
                         <option value="0">* Select Professional Status</option>
@@ -208,7 +217,7 @@ const CreateProfile = props => {
 }
 
 CreateProfile.propTypes = {
-
+    createProfile: PropTypes.func.isRequired,
 }
 
-export default CreateProfile
+export default connect(null, { createProfile })(withRouter(CreateProfile))
