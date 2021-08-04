a8c9ee4e1df4a8cd7fd90644c4c55d0efd053ebf
Chujun Lu <chujunlu@hotmail.com>
2021-08-04 21:16:34 +0800

    Add component to display github repos

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import ProfileTop from './ProfileTop'
 import ProfileAbout from './ProfileAbout'
 import ProfileExperience from './ProfileExperience'
 import ProfileEducation from './ProfileEducation'
+import ProfileGithub from './ProfileGithub'
 import Spinner from '../layout/Spinner'
 import { getProfileById } from '../../actions/profile'
 
@@ -66,6 +67,10 @@ const Profile = ({
                                 <h4>No education credentials</h4>
                             )}
                         </div>
+
+                        {profile.githubusername && (
+                            <ProfileGithub username={profile.githubusername} />
+                        )}
                     </div>
             </>}
         </>
