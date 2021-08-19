dd60d31ffe18dab725b9fa09a5d639299555191d
Chujun Lu <chujunlu@hotmail.com>
2021-08-19 17:09:09 +0800

    Use redux subscription to manage local storage

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 const setAuthToken = token => {
     if (token) {
+        localStorage.setItem('token', token);
         axios.defaults.headers.common['x-auth-token'] = token;
     } else {
+        localStorage.removeItem('token');
         delete axios.defaults.headers.common['x-auth-token'];
     }
 }
